a7d55e7f8ab06037b77e873fed206ef9c2ec6c43
mboultoureau <contact@mboultoureau.com>
2022-12-12 09:18:13 +0100

    Add 2022 - Day 7

---
+++ 2022/day7/part1.py
@@ -0,0 +1,95 @@
+import re
+import pprint
+
+f = open("input2.txt", "r")
+lines = f.readlines()
+current_directory = ["/"]
+structure = {
+    "/": {}
+}
+
+for i in range(len(lines)):
+    line = lines[i].strip()
+
+    x = re.search(r"^\$ cd \/$", line)
+    if x != None:
+        current_directory = ["/"]
+        print("Change current directory to /")
+        continue
+
+    x = re.search(r"^\$ cd ([a-zA-Z]+)$", line)
+    if x != None:
+        current_directory.append(x.group(1))
+        print("Change current directory to", x.group(1))
+        continue
+
+    x = re.search(r"^\$ cd \.\.$", line)
+    if x != None:
+        current_directory.pop()
+        print("Change current directory to ..")
+        continue
+
+    x = re.search(r"^\$ ls$", line)
+    if x != None:
+        print("List files and directories contained by current directory")
+        continue
+
+    x = re.search(r"^dir ([a-zA-Z]+)$", line)
+    if x != None:
+        dir = structure
+        for d in current_directory:
+            dir = dir[d]
+
+        dir[x.group(1)] = {}
+        print("Directory", x.group(1), "found")
+        continue
+
+    x = re.search(r"^([0-9]+) ([a-zA-Z\.]+)$", line)
+    if x != None:
+        dir = structure
+        for d in current_directory:
+            dir = dir[d]
+
+        dir[x.group(2)] = int(x.group(1))
+        print("File", x.group(2), "with size of", x.group(1), "bytes found")
+        continue
+
+    raise
+
+
+# Print structure
+def print_structure(dir, indent = 0):
+    indentation = indent * '  '
+
+    for key in dir:
+        if type(dir[key]) is dict:
+            print(f"{indentation}- {key} (dir)")
+            print_structure(dir[key], indent + 1)
+        else:
+            print(f"{indentation} - {key} (file, size={dir[key]})")
+
+def calculate_size(dir, indent = 0):
+    sum = 0
+    indentation = indent * '  '
+
+    for key in dir:
+        # print(key, sum)
+        if type(dir[key]) is dict:
+            print(f"{indentation}- {key} (dir)")
+            sum_dir = calculate_size(dir[key], indent + 1)
+            sum += sum_dir
+
+            global total
+            if sum_dir <= 100000:
+                total += sum_dir
+
+        else:
+            print(f"{indentation} - {key} (file, size={dir[key]})")
+            sum += dir[key]
+    
+    return sum
+
+# print_structure(structure, 0)
+total = 0
+calculate_size(structure, 0)
+print(f"Total: {total}")
